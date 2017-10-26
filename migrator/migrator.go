8e6c176945935e64a09b67940005d978b46855e3
Sem Rekkers <rekkers.sem@gmail.com>
2017-10-26 22:08:13 +0200

    Add migrator and MySQL bindings

---
+++ migrator/migrator.go
@@ -0,0 +1,93 @@
+package migrator
+
+import "github.com/semrekkers/sugar"
+
+const (
+	Initialize = ""
+)
+
+type Model interface {
+	TableName() string
+	Migrate(*sugar.Querier, string)
+}
+
+type DBInfo interface {
+	HasTable(sugar.Executor, string) (bool, error)
+	TableColumns(sugar.Executor, string) ([]string, error)
+}
+
+type Migrator struct {
+	ex      sugar.Executor
+	bindVar sugar.Formatter
+	mapper  sugar.TypeMapper
+	dbInfo  DBInfo
+}
+
+type MigrationResult struct {
+	Tables, Columns []string
+}
+
+func New(ex sugar.Executor, bindVar sugar.Formatter, mapper sugar.TypeMapper, dbInfo DBInfo) *Migrator {
+	return &Migrator{ex, bindVar, mapper, dbInfo}
+}
+
+func NewFromDB(db *sugar.DB, dbInfo DBInfo) *Migrator {
+	return &Migrator{db.DB, db.BindVar, db.Mapper, dbInfo}
+}
+
+func (m *Migrator) Migrate(models ...Model) (*MigrationResult, error) {
+	var result MigrationResult
+
+	for _, model := range models {
+		tableName := model.TableName()
+
+		tableExists, newColumns, err := m.migrateModel(tableName, model)
+		if err != nil {
+			return nil, err
+		}
+		if tableExists {
+			result.Tables = append(result.Tables, tableName)
+		} else {
+			result.Columns = append(result.Columns, newColumns...)
+		}
+	}
+
+	return &result, nil
+}
+
+func (m *Migrator) migrateModel(tableName string, model Model) (tableExists bool, newColumns []string, err error) {
+	tableExists, err = m.dbInfo.HasTable(m.ex, tableName)
+	if err != nil {
+		return
+	}
+
+	q := sugar.NewQuerier(m.ex, sugar.DefaultBindVar)
+	fieldSelector := sugar.Fields(model).SetTypeMapper(m.mapper)
+	if !tableExists {
+		q.Writef("CREATE TABLE %s (", tableName)
+		q.FieldDefinitions("", fieldSelector.Select())
+		model.Migrate(q, Initialize)
+		q.WriteString(")")
+	} else {
+		var columns []string
+		columns, err = m.dbInfo.TableColumns(m.ex, tableName)
+		if err != nil {
+			return
+		}
+		fields := fieldSelector.Except(columns...).Select()
+		if len(fields) == 0 {
+			return
+		}
+
+		q.Writef("ALTER TABLE %s (", tableName)
+		q.FieldDefinitions("ADD ", fields)
+		for _, field := range fields {
+			model.Migrate(q, field.Name)
+			newColumns = append(newColumns, tableName+"."+field.Name)
+		}
+		q.WriteString(")")
+	}
+	err = q.Exec()
+
+	return
+}
---
+++ mysql/mysql.go
@@ -0,0 +1,44 @@
+package mysql
+
+import (
+	"reflect"
+
+	"github.com/semrekkers/sugar"
+
+	go_mysql "github.com/go-sql-driver/mysql"
+)
+
+var (
+	reflectTypeNullTime = reflect.TypeOf(go_mysql.NullTime{})
+)
+
+func TypeMapper(t reflect.Type) (out string, ok bool) {
+	out, ok = sugar.DefaultTypeMapper(t)
+	if !ok {
+		switch t {
+		case reflectTypeNullTime:
+			ok, out = true, "DATETIME NULL"
+		}
+	}
+	return
+}
+
+type DBInfo struct{}
+
+func (_ DBInfo) HasTable(db *sugar.DB, tableName string) (tableExists bool, err error) {
+	err = db.Querier().
+		Write("SELECT EXISTS ( SELECT table_name FROM information_schema.tables WHERE table_schema = (SELECT DATABASE())").
+		Write("AND table_name = ? )", tableName).
+		Scan(&tableExists)
+
+	return
+}
+
+func (_ DBInfo) TableColumns(db *sugar.DB, tableName string) (columns []string, err error) {
+	err = db.Querier().
+		Write("SELECT EXISTS ( SELECT column_name FROM information_schema.columns WHERE table_schema = (SELECT DATABASE())").
+		Write("AND table_name = ? )", tableName).
+		ForEach(sugar.AppendToStringSlice(&columns))
+
+	return
+}
